b6a78c36fe399cf2158446d34f8c91f359808cd9
balamurugan-04 <67495513+balamurugan-04@users.noreply.github.com>
2022-03-14 15:06:37 +0530

    final commit

---
+++ house-marketplace/src/components/ListingItem.jsx
@@ -0,0 +1,51 @@
+import { Link } from "react-router-dom";
+import { ReactComponent as DeleteIcon } from "../assets/svg/deleteIcon.svg";
+import { ReactComponent as EditIcon } from "../assets/svg/editIcon.svg";
+import bedIcon from "../assets/svg/bedIcon.svg";
+import bathtubIcon from "../assets/svg/bathtubIcon.svg";
+
+const ListingItem = ({ listing, id, onDelete, onEdit }) => {
+  return (
+    <li className="categoryListing">
+      <Link
+        to={`/category/${listing.type}/${id}`}
+        className="categoryListingLink"
+      >
+        <img
+          src={listing.imageUrls ? listing.imageUrls[0] : listing.imgUrls[0]}
+          alt={listing.name}
+          className="categoryListingImg"
+        />
+        <div className="categoryListingDetails">
+          <p className="categoryListingLocation">{listing.location}</p>
+          <p className="categoryListingName">{listing.name}</p>
+          <p className="categoryListingPrice">
+            ${listing.offer ? listing.discountedPrice : listing.regularPrice}
+            {listing.type === "rent" && " / Month"}
+          </p>
+          <div className="categoryListingInfoDiv">
+            <img src={bedIcon} alt="bed" />
+            <p className="categoryListingInfoText">
+              {listing.bedrooms + " Bedrooms"}
+            </p>
+            <img src={bathtubIcon} alt="bathtub" />
+            <p className="categoryListingInfoText">
+              {listing.bathrooms + " Bathrooms"}
+            </p>
+          </div>
+        </div>
+      </Link>
+      {onDelete && (
+        <DeleteIcon
+          className="removeIcon"
+          fill="rgb(231, 76,60)"
+          onClick={() => onDelete(listing.id, listing.name)}
+        />
+      )}
+
+      {onEdit && <EditIcon className="editIcon" onClick={() => onEdit(id)} />}
+    </li>
+  );
+};
+
+export default ListingItem;
---
+++ house-marketplace/src/firebase.config.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC78zxbLwfB0VKGqlZCkX0-IEMGOTLtRmk",
+  authDomain: "house-marketplace-app-77e92.firebaseapp.com",
+  projectId: "house-marketplace-app-77e92",
+  storageBucket: "house-marketplace-app-77e92.appspot.com",
+  messagingSenderId: "1007503717560",
+  appId: "1:1007503717560:web:db6e4045663442487c704d",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore();
